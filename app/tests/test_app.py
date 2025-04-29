b119af7f6e37f300131d476ec871163d2ee7d8af
Sidivete <135848641+Sidivete@users.noreply.github.com>
2025-04-29 16:17:00 +0200

    update pour teste route predict

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -6,3 +6,14 @@ client = TestClient(app)
 def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
+
+def test_predict_keywords():
+    data = {
+        "text": "Le machine learning permet de prédire des résultats.",
+        "max_keywords": 3
+    }
+    response = client.post("/predict", json=data)
+    assert response.status_code == 200
+    result = response.json()
+    assert "keywords" in result
+    assert len(result["keywords"]) <= 3
