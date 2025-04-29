99cb246316ec2a2b2d97c64a7c2ad373c3d66096
Sidivete <135848641+Sidivete@users.noreply.github.com>
2025-04-29 17:28:37 +0200

    pour afficher la prediction

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -9,4 +9,7 @@ def test_predict_keywords():
         }
         response = client.post("/predict", json=data)
         print("DEBUG:", response.status_code, response.json())
+         # Afficher le statut et le contenu de la réponse
+        print(f"Status Code: {response.status_code}")
+        print(f"Response JSON: {response.json()}")  # Affiche les prédictions dans les logs
         assert response.status_code == 200
