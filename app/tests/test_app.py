c6c68edc982ae2665d78ad819d6ec9a5fcdc8fc3
Sidivete <135848641+Sidivete@users.noreply.github.com>
2025-04-29 16:57:17 +0200

    predict test

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,19 +1,12 @@
 from fastapi.testclient import TestClient
 from app.main import app
 
-client = TestClient(app)
-
-def test_health_check():
-    response = client.get("/health")
-    assert response.status_code == 200
-
 def test_predict_keywords():
-    data = {
-        "text": "Le machine learning permet de prédire des résultats.",
-        "max_keywords": 3
-    }
-    response = client.post("/predict", json=data)
-    assert response.status_code == 200
-    result = response.json()
-    assert "keywords" in result
-    assert len(result["keywords"]) <= 3
+    with TestClient(app) as client:  # 👈 Ce "with" est important
+        data = {
+            "text": "Le machine learning permet de prédire des résultats.",
+            "max_keywords": 3
+        }
+        response = client.post("/predict", json=data)
+        print("DEBUG:", response.status_code, response.json())
+        assert response.status_code == 200
